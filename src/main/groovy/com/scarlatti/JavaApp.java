7b0c3c6aeb64ae889ca20aa7e4ae7333197d4327
alessandroscarlatti <violanotesnoextras@gmail.com>
2017-10-30 14:56:46 -0500

    added support for Thymeleaf web application.

---
+++ src/main/groovy/com/scarlatti/JavaApp.java
@@ -0,0 +1,14 @@
+package com.scarlatti;
+
+/**
+ * Created by pc on 10/25/2017.
+ */
+public class JavaApp {
+    public static void main(String[] args) {
+        new JavaApp().run();
+    }
+
+    public void run() {
+        System.out.println("hello Java!");
+    }
+}
---
+++ src/main/groovy/com/scarlatti/controllers/RootController.java
@@ -0,0 +1,44 @@
+package com.scarlatti.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * ~     _____                                    __
+ * ~    (, /  |  /)                /)         (__/  )      /)        ,
+ * ~      /---| // _ _  _  _ __  _(/ __ ___     / _ _  __ // _ _/_/_
+ * ~   ) /    |(/_(//_)/_)(_(/ ((_(_/ ((_)   ) / (_(_(/ ((/_(_((_(__(_
+ * ~  (_/                                   (_/
+ * ~  Monday, 10/30/2017
+ */
+@Controller
+public class RootController {
+
+    /**
+     * Easier to test this method declaration style,
+     * since the Model is easily mocked and
+     * the view returned is easily validated.
+     *
+     * @param m the model to be sent to the template
+     * @return the view for this endpoint.
+     */
+    @GetMapping("/")
+    public String index(Model m) {
+        return "index";
+    }
+
+    /**
+     * Fill in the model from information in the request params.
+     *
+     * @param name name from request params
+     * @param m model to be sent to the template
+     * @return the view for this endpoint
+     */
+    @GetMapping("/api/greeting")
+    public String api(@RequestParam(value="name", defaultValue="World") String name, Model m) {
+        m.addAttribute("name", name);
+        return "api";
+    }
+}
